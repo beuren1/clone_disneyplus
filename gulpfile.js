abbbfb0e799fc95c703014eaf88b6fd1d762436b
Josué Beuren <josuebeurensilvaa@gmail.com>
2025-08-06 13:00:46 -0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
+// Compila SCSS em CSS
 function styles() {
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+// Otimiza imagens
+function images() {
+    return gulp.src('./src/images/**/*.{png,jpg,jpeg,svg,gif}')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
+
+// Task padrão
+exports.default = gulp.parallel(styles, images);
+
+// Watch opcional
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', styles);
+    gulp.watch('./src/images/**/*.{png,jpg,jpeg,svg,gif}', images);
+};
