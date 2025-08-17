4789c73934709a84b96c5d9f0cf99440395263f4
Josué Beuren <josuebeurensilvaa@gmail.com>
2025-08-16 22:00:40 -0300

    finalizando

--- src/main.js
+++ src/main.js
@@ -1,18 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function () {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    });
+
+    // Troca de abas
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function (botao) {
-            const abaAlvo = botao.target.dataset.tabButton;
+        buttons[i].addEventListener('click', function (event) {
+            const abaAlvo = event.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
 
             escondeTodasAbas();
             removeBotaoAtivo();
 
             aba.classList.add('shows__list--is-active');
-            botao.target.classList.add('shows__tab__button--is-active');
+            event.target.classList.add('shows__tab__button--is-active');
         });
+    }
+
+    // FAQ - abre e fecha respostas
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
     }
 
     function escondeTodasAbas() {
@@ -22,9 +42,24 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function removeBotaoAtivo() {
-        const botoes = document.querySelectorAll('[data-tab-button]');
-        for (let i = 0; i < botoes.length; i++) {
-            botoes[i].classList.remove('shows__tab__button--is-active');
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].classList.remove('shows__tab__button--is-active');
         }
     }
+
+    function abreOuFechaResposta(event) {
+        const classe = 'faq__questions__item--is-open';
+        const elementoPai = event.target.parentNode;
+        elementoPai.classList.toggle(classe);
+    }
 });
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
